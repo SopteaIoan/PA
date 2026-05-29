b59451b1d151aa93c730532b36a683331372a4d4
SopteaIoan <ioan.soptea@student.tuiasi.ro>
2026-05-29 09:16:24 +0300

    Update functii.cpp

--- lab6/functii.cpp
+++ lab6/functii.cpp
@@ -22,7 +22,7 @@ void rucsac(obiect o[10],int n,int M,double x[]) {
     }
 }
 
-void bubblesort(obiect o[10], int n) {
+void bubblesort(obiect o[10],int n) {
     for (int i=0;i<n-1;i++) {
         for (int j=0;j<n-i-1;j++) {
             if (o[j].r<o[j+1].r) 
