8493a692cc1e7771d10fc590d353ab728cbb3ce7
SopteaIoan <ioan.soptea@student.tuiasi.ro>
2026-05-29 09:21:48 +0300

    Update header.h

--- lab5/header.h
+++ lab5/header.h
@@ -1,19 +1,21 @@
 #ifndef ceva
 #define ceva
 
-struct Node {
+struct Node
+{
     int pondere;
     Node *stanga, *dreapta;
 };
 
 Node *createnode(int p);
 
-struct ComparaNoduri {
+struct ComparaNoduri
+{
     bool operator()(Node* a,Node* b) {
         return a->pondere > b->pondere;
     }
 };
 
-int calculeazaLEP(Node* radacina,int nivel);
+int calculeazaLEP(Node *radacina,int nivel);
 
 #endif
